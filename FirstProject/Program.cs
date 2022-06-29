f5c2632bb1cf1eabcd658b6eaf06026a55e70d46
Sifiso Makhaye <sifiso.makhaye@sage.com>
2022-06-29 11:07:26 +0200

    Add project files.

---
+++ FirstProject/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using FirstProject.Calc;
+
+namespace FirstProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] aOperator = new int[4]{ '+', '-', '*', '/' };
+
+             Console.WriteLine("What type of calculation do you want to perform? (+, -, *, /)");
+
+            string read = Console.ReadLine();
+
+
+
+
+            /*Console.WriteLine("Write the first number");
+            string num1 = Console.ReadLine();
+
+            Console.WriteLine("Write the second number");
+            string num2 = Console.ReadLine();
+
+            Console.WriteLine("The answer is"+ answer);
+
+            Console.WriteLine("Write 'yes' to restart application");
+
+             Console.WriteLine("Choose a valid operator type");
+
+             Console.WriteLine("Try again! This time with actual numbers... -_-*");
+             
+             */
+
+
+
+        }
+    }
+}
