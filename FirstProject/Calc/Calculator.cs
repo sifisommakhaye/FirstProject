47e96a66b76be77f8f3ae8701ac8432180b161a2
Sifiso Makhaye <sifiso.makhaye@sage.com>
2022-06-29 21:17:31 +0200

    adding methods

--- FirstProject/Calc/Calculator.cs
+++ FirstProject/Calc/Calculator.cs
@@ -1,42 +1,100 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace FirstProject.Calc
 {
     public class Calculator
     {
-        // adding method
-        public static int Add(int x, int y) 
+        public void RunCalculation()
         {
-            int num1 = x;
-            int num2 = y;
-            int answer = num1 + num2;
-            return answer;
+            // declare variables
+            string[] allowedOper = new string[4] { "+", "-", "*", "/" };
+            string type;
+            int num1;
+            int num2;
+            int answer;
+            string restartCheck;
+
+            // get the operator type
+            Console.WriteLine("What type of calculation do you want to perform? (+, -, *, /)");
+            type = GetCalcType(allowedOper);
+
+
+            Console.WriteLine("Write the first number.");
+            num1 = GetCalcNum();
+
+            Console.WriteLine("Write the second number");
+            num2 = GetCalcNum();
+
+            answer = Calculate(num1, num2, type);
+
+            Console.WriteLine("The answer is" + answer + " \r\n Write 'yes' to restart application");
+
+            restartCheck = Console.ReadLine();
+            if (restartCheck == "yes")
+            {
+                RunCalculation();
+            }
+            else
+            {
+                Environment.Exit(0);
+            }
         }
 
-        public static int Subract(int x, int y)
+        private string GetCalcType(string[] allowedOper)
         {
-            int num1 = x;
-            int num2 = y;
-            int answer = num1 + num2;
-            return answer;
+            string type = Console.ReadLine();
+
+            while (!allowedOper.Contains(type))
+            {
+                Console.WriteLine("Choose a valid operator type!");
+                type = Console.ReadLine();
+            }
+
+            return type;
+        }
+        private int GetCalcNum()
+        {
+            int num;
+
+            bool parseCheck = Int32.TryParse(Console.ReadLine(), out num);
+
+            while (!parseCheck)
+            {
+                Console.WriteLine("Try again! This time with actual numbers...");
+
+                parseCheck = Int32.TryParse(Console.ReadLine(), out num);
+            }
+            return num;
         }
 
-        public static int Multiply(int x, int y)
+        public int Calculate(int num1, int num2, string type)
         {
-            int num1 = x;
-            int num2 = y;
-            int answer = num1 + num2;
-            return answer;
-        }
+            int result;
 
-        public static int Divide(int x, int y)
-        {
-            int num1 = x;
-            int num2 = y;
-            int answer = num1 + num2;
-            return answer;
+            if (type == "+")
+            {
+                result = num1 + num2;
+                return result;
+            }
+            else if (type == "-")
+            {
+                result = num1 - num2;
+                return result;
+            }
+            else if (type == "*")
+            {
+                result = num1 * num2;
+                return result;
+            }
+            else
+            {
+                result = num1 / num2;
+                return result;
+            }
+
         }
-    }
+    }   
 }
--- FirstProject/Program.cs
+++ FirstProject/Program.cs
@@ -7,31 +7,8 @@ namespace FirstProject
     {
         static void Main(string[] args)
         {
-            int[] aOperator = new int[4]{ '+', '-', '*', '/' };
-
-             Console.WriteLine("What type of calculation do you want to perform? (+, -, *, /)");
-
-            string read = Console.ReadLine();
-
-
-
-
-            /*Console.WriteLine("Write the first number");
-            string num1 = Console.ReadLine();
-
-            Console.WriteLine("Write the second number");
-            string num2 = Console.ReadLine();
-
-            Console.WriteLine("The answer is"+ answer);
-
-            Console.WriteLine("Write 'yes' to restart application");
-
-             Console.WriteLine("Choose a valid operator type");
-
-             Console.WriteLine("Try again! This time with actual numbers... -_-*");
-             
-             */
-
+            Calculator calculator = new Calculator();
+            calculator.RunCalculation();
 
         }
     }
